c0dfad52d3baa55922495ab69b05b8423914a7e3
Andy Pernsteiner <andypern@gmail.com>
2016-06-14 16:44:11 -0700

    testing cs logic

--- Perf_Tests/python/perf_suite_multip.py
+++ Perf_Tests/python/perf_suite_multip.py
@@ -19,13 +19,17 @@ import pprint
 from datetime import datetime
 import logging
 
-from functools import partial
-from multiprocessing.pool import Pool
 
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logging.getLogger('requests').setLevel(logging.CRITICAL)
-logger = logging.getLogger(__name__)
+from multiprocessing import Process, JoinableQueue, Queue
+from progressbar import ProgressBar, SimpleProgress
+
+
+
+
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# logging.getLogger('requests').setLevel(logging.CRITICAL)
+# logger = logging.getLogger(__name__)
 
 
 
@@ -87,81 +91,120 @@ if len(opts) < 5:
     sys.exit(1)
 
 
+class fileUploader(Process):
+
+    def __init__(self, file_queue, result_queue, endpoint, bucket, access_key, secret_key):
+        Process.__init__(self)
+        self.file_queue = file_queue
+        self.result_queue = result_queue
+        self.endpoint = endpoint
+        self.bucketname = bucket
+        self.access_key = access_key
+        self.secret_key = secret_key
+
+    def run(self):
+        proc_name = self.name
+
+        transfer, client, config = make_session(self.endpoint, self.access_key, self.secret_key)
+        
+        while True:
+            next_task = self.file_queue.get()
+            print("{}: Queue String: {}".format(proc_name,next_task))
+        
+            if next_task is None:
+                self.file_queue.task_done()
+                break
+            #print("file_name {}".format(next_task[0]))
+            
+            
+            upload_result = upload_file(self.bucketname, next_task,transfer)
+            self.result_queue.put(upload_result)
+
+        #print("{}: Queue String: {}".format(proc_name,next_task))
+        #print("Exiting {}".format(proc_name))
+        return
 
 
-def make_session():
 
+def make_session(endpoint,access_key,secret_key):
 
     session = boto3.session.Session()
 
-
-    client = session.client('s3',
-        aws_access_key_id = access_key,
-        aws_secret_access_key = secret_key,
-        endpoint_url='http://' + endpoint,
-        use_ssl=False,
-        verify=False,
-        config=boto3.session.Config(
-            signature_version='s3',
-            connect_timeout=60,
-            read_timeout=60,
-            s3={'addressing_style': 'path'})
-            )
-
-    return client
+    try:
 
 
+        client = session.client('s3',
+            aws_access_key_id = access_key,
+            aws_secret_access_key = secret_key,
+            endpoint_url='http://' + endpoint,
+            use_ssl=False,
+            verify=False,
+            config=boto3.session.Config(
+                signature_version='s3',
+                connect_timeout=60,
+                read_timeout=60,
+                s3={'addressing_style': 'path'})
+                )
 
-
-def upload_file(objKey, bucket, client):
-
-    config = TransferConfig(
-    multipart_threshold= 20 * 1024 * 1024 * 1024,  # Set threshold to 8GB before using Multipart
-    max_concurrency=10,
+        config = boto3.s3.transfer.TransferConfig(
+            multipart_threshold=20 * 1024 * 1024 * 1024, 
+            max_concurrency = 10,
         )
 
-    transfer_client = S3Transfer(client,config)
+        transfer = boto3.s3.transfer.S3Transfer(client, config)
 
-    #print transfer
-    print "ya"
-    #print "%s in upload_file" %(objKey)
+    except botocore.exceptions.ClientError as e:
+        error_code = int(e.response['Error']['Code'])
+        print("Errorcode {} Connecting to {} : with access key {} : secret key {}".
+            format(error_code, endpoint, access_key, secret_key))
+        sys.exit(1)
+
+    return transfer,client,config
+
+
+def upload_file(bucket, objKey, transfer):
+
+    print "%s in upload_file" %(objKey)
     local_start_time = time.time()
-    # try:    
-    #     response = transfer.upload_file(
-    #         inputfile, 
-    #         bucket,
-    #         objKey
-    #         )
-    #     local_elapsed_time = time.time() - local_start_time
-    #     #print "at %s made file # %s in %s" %(nowtime, i, local_elapsed_time)
-    # except botocore.exceptions.ClientError as e:
-    #     printfail(method,e.response)
-    # except (botocore.vendored.requests.exceptions.ConnectionError, 
-    #     botocore.vendored.requests.exceptions.ReadTimeout) as e:
-    #     #if we get a timeout, retry until we hit max_retries
-    #     print "got a timeout for %s: %s" %(objKey,e)
-    #     local_elapsed_time = time.time() - local_start_time
-    #     print "this timeout took %s , initiating retry" %(local_elapsed_time)
+    try:    
+        response = transfer.upload_file(
+            inputfile, 
+            bucket,
+            objKey
+            )
+        local_elapsed_time = time.time() - local_start_time
+        #print "at %s made file # %s in %s" %(nowtime, i, local_elapsed_time)
+    except botocore.exceptions.ClientError as e:
+        printfail(method,e.response)
+    except (botocore.vendored.requests.exceptions.ConnectionError, 
+        botocore.vendored.requests.exceptions.ReadTimeout) as e:
+        #if we get a timeout, retry until we hit max_retries
+        print "got a timeout for %s: %s" %(objKey,e)
+        local_elapsed_time = time.time() - local_start_time
+        print "this timeout took %s , initiating retry" %(local_elapsed_time)
 
-    #     for attempt in range(max_retries):
-    #         print "attempt #%s of %s" %(attempt + 1,max_retries)
-    #         try:
-    #             response =  transfer.upload_file(
-    #                 inputfile, 
-    #                 bucket,
-    #                 objKey
-    #                 )
-    #             local_elapsed_time = time.time() - local_start_time
-    #             #print "at %s made file # %s in %s" %(nowtime, i, local_elapsed_time)
-    #             break
-    #         except (botocore.vendored.requests.exceptions.ConnectionError, 
-    #                 botocore.vendored.requests.exceptions.ReadTimeout) as e:
-    #             if attempt < (max_retries - 1):
-    #                 print "attempt %s failed, %s remaining" %(attempt + 1, max_retries - attempt)
-    #                 continue
-    #             else:
-    #                 print "all attempts failed..aborting this transfer"
-    #                 break
+        for attempt in range(max_retries):
+            print "attempt #%s of %s" %(attempt + 1,max_retries)
+            try:
+                response =  transfer.upload_file(
+                    inputfile, 
+                    bucket,
+                    objKey
+                    )
+                local_elapsed_time = time.time() - local_start_time
+                #print "at %s made file # %s in %s" %(nowtime, i, local_elapsed_time)
+                break
+            except (botocore.vendored.requests.exceptions.ConnectionError, 
+                    botocore.vendored.requests.exceptions.ReadTimeout) as e:
+                if attempt < (max_retries - 1):
+                    print "attempt %s failed, %s remaining" %(attempt + 1, max_retries - attempt)
+                    continue
+                else:
+                    print "all attempts failed..aborting this transfer"
+                    return objKey + ",bad"
+                    break
+
+    return objKey + ",good"
 
 def delete_object(s3client,bucket,objKey):
     method = 'delete_object'
@@ -187,13 +230,14 @@ def list_objects(s3client,bucket, fileprefix):
 
         pages = paginator.paginate(
             Bucket=bucket,
-            Delimiter='/',
+            #Delimiter='/',
             Prefix=fileprefix
         )
-
+        print bucket
         return pages
     except botocore.exceptions.ClientError as e:
         printfail(method,e.response)
+
 
 
 
@@ -233,25 +277,67 @@ def main():
 
     ts = time.time()
 
+    file_queue = JoinableQueue()
+    result_queue = Queue()
 
-    # client_id = os.getenv('IMGUR_CLIENT_ID')
-    # if not client_id:
-    #     raise Exception("Couldn't find IMGUR_CLIENT_ID environment variable!")
-    # download_dir = setup_download_dir()
-    # links = (l for l in get_links(client_id) if l.endswith('.jpg'))
-    # download = partial(download_link, download_dir)
+    file_processes = [ fileUploader(file_queue, result_queue, endpoint, bucket, access_key, secret_key)
+                      for i in range(threadcount) ]    
 
-    client = make_session()
-    upload = partial(upload_file,bucket,client)
     fileList = []
     for f in range(filecount):
         objKey = fileprefix + str(f) + '-' + (''.join(choice(ascii_uppercase) for i in range(6)))
         fileList.append(objKey)
 
 
-    p = Pool(threadcount)
-    p.map(upload, fileList)
-    logging.info('Took %s seconds', time() - ts)
+
+    for w in file_processes:
+        w.start()
+
+    start_time = time.time()
+    
+    for objKey in fileList:
+        file_queue.put(objKey)
+      
+     #
+    #this is sort of ghetto..add a None to the queue so that when processing we can tst for it to know 
+    # that we're at the end of the queue..must be a better way
+    #
+
+    for i in range(threadcount):
+        file_queue.put(None)
+
+    print("Waiting for threads to stop")
+    for w in file_processes:
+        w.join()
+
+        delta = time.time() - ts
+    print('Took {}'.format(delta))
+
+    #
+    #rough calculations
+    #
+    totalBytes = fsize * filecount
+    bytesSec = totalBytes / delta
+    MBytesec = totalBytes / (1024 * 1024) / delta
+    print "%s MB/sec" %(MBytesec)
+
+    #now delete
+    #
+    #get a list of objects in pages, then blow them away
+    #
+    s3client = make_session(endpoint, access_key, secret_key)[1]
+
+    objList =  list_objects(s3client,bucket, fileprefix)
+
+    count = 0
+    for page in objList:
+            for key in page['Contents']:
+                delete_object(s3client,bucket,key['Key'])
+                count += 1
+
+
+    print "deleted %s" %(count)
+
 
 
 if __name__ == '__main__':
