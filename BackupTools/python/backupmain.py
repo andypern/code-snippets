e37ba45553fe29e01b8fb93bd16b2a4fc3395471
Andy Pernsteiner <andypern@gmail.com>
2016-06-27 13:28:23 -0400

    updated from cs' edits

--- BackupTools/python/backupmain.py
+++ BackupTools/python/backupmain.py
@@ -10,13 +10,15 @@ import botocore
 import mimetypes
 from multiprocessing import Process, JoinableQueue, Queue
 
-#https://docs.python.org/2/library/multiprocessing.html
-#https://pymotw.com/2/multiprocessing/communication.html
+# https://pymotw.com/2/multiprocessing/basics.html
 
+# class for managing the threads that are spawned,  filenames are passed into the 
+# thread through the a joinable queue,  the filesize that is uploaded is returned
+# via another queue that ends up caculating total system throughput.
+class Fileuploader(Process):
 
-class fileUploader(Process):
-
-	def __init__(self, file_queue, result_queue, endpoint, bucketname, access_key, secret_access_key):
+	def __init__(self, file_queue, result_queue, endpoint, 
+			bucketname, access_key, secret_access_key):
 		Process.__init__(self)
 		self.file_queue = file_queue
 		self.result_queue = result_queue
@@ -26,22 +28,31 @@ class fileUploader(Process):
 		self.secret_access_key = secret_access_key
 
 	def run(self):
-		proc_name = self.name
 
+		# set the threadname for use in debugging
+		proc_name = self.name
+		
+		#Call the Connect function to setup the s3 connection, each thread makes its own connection
 		transfer, client, config = Connect(self.endpoint, self.access_key, self.secret_access_key)
-		
+	
+		# Listen and loop on the queue until it hits a None and then gracefully shutdown	
 		while True:
+			# Get the next item in the queue
 			next_task = self.file_queue.get()
+
 			print("{}: Queue String: {}".format(proc_name,next_task))
-		
+	
+			# if the non market is hit, gracefully shutdown the thread	
 			if next_task is None:
 				print("Exiting: {}".format(proc_name))
 				self.file_queue.task_done()
 				break
+
 			print(" dir_name {}, file_name {}".format(next_task[0], next_task[1]))
 			
-			
+			# upload the object into S3. 			
 			file_size = Upload(self.bucketname, next_task[0], next_task[1], transfer)
+			# put the size of the object into the results queue for processing later
 			self.result_queue.put(file_size)
 
 		print("{}: Queue String: {}".format(proc_name,next_task))
@@ -80,27 +91,28 @@ def Uploadfile(bucketname, directory, full_file_name, transfer, stat_info, mime_
 	try: 
 		transfer.upload_file(full_file_name, 
 					bucketname, 
-					full_file_name_woslash,
-					extra_args={"Metadata":{'backup-date'		: time.ctime(),
-								'meta-ign-u-mode'	: str(stat_info.st_mode),
-								'meta-ign-u-uid'	: str(stat_info.st_uid),
-								'meta-ign-u-gid'	: str(stat_info.st_gid),
-								'meta-ign-c-atime'	: str(stat_info.st_atime),
-								'meta-ign-c-mtime'	: str(stat_info.st_mtime),
-								'meta-ign-c-ctime'	: str(stat_info.st_ctime),
-								'meta-ign-mimetype'	: mime_type,
-								'meta-ign-encode'	: encode_type,
-								'meta-ign-extension'	: extension_name
-								}})
+					full_file_name_woslash
+					#extra_args={"Metadata":{'backup-date'		: time.ctime(),
+					#			'meta-ign-u-mode'	: str(stat_info.st_mode),
+					#			'meta-ign-u-uid'	: str(stat_info.st_uid),
+					#			'meta-ign-u-gid'	: str(stat_info.st_gid),
+					#			'meta-ign-c-atime'	: str(stat_info.st_atime),
+					#			'meta-ign-c-mtime'	: str(stat_info.st_mtime),
+					#			'meta-ign-c-ctime'	: str(stat_info.st_ctime),
+					#			'meta-ign-mimetype'	: mime_type,
+					#			'meta-ign-encode'	: encode_type,
+					#			'meta-ign-extension'	: extension_name
+					#			}}
+					)
 		
 	except botocore.exceptions.ClientError as e:
-		error_code = int(e.response['Error']['Code'])
-		print("Errorcode {} Connecting to {} : with access key {} : secret key {}".
-			format(error_code, endpoint, access_key, secret_access_key))
+		error_code = e.response['Error']['Code']
+		print("Upload File Error : ErrorCode >{}< Unexpected error {} {}".format(error_code, e,e.response['Error']['Code']))
+		print("Upload File Error : Filename {} : Full Name of File {}".format(full_file_name, full_file_name_woslash))
 		sys.exit(1)
 
-	except IOError as e:
-		print("I/O error {0}:{1}".format(e.errno, e.strerror))
+	except IOError as f:
+		print("I/O error {0}:{1}".format(f.errno, f.strerror))
 
 	
 	
@@ -149,9 +161,8 @@ def Connect(endpoint, access_key, secret_access_key):
 		transfer = boto3.s3.transfer.S3Transfer(client, config) 
 
 	except botocore.exceptions.ClientError as e:
-		error_code = int(e.response['Error']['Code'])
-		print("Errorcode {} Connecting to {} : with access key {} : secret key {}".
-			format(error_code, endpoint, access_key, secret_access_key))
+		print("Unexpected error {}".format(e))
+		print("Cannot connect to S3 endpoint")
 		sys.exit(1)
 
 	except IOError as f:
@@ -233,14 +244,13 @@ if __name__ == '__main__':
 		print('Could not open /tmp/zerofile.0')
 		sys.exit(1)
 
-
-	#file_queue = Queue()
 	file_queue = JoinableQueue()
 	result_queue = Queue()
+
 	print("Directory {}: num_threads {}: endpoint {}: access_key {}: secret_access_key {}"
 			.format(directory_path, num_threads, endpoint, access_key, secret_access_key))
 
-	file_processes = [ fileUploader(file_queue, result_queue, endpoint, bucketname, access_key, secret_access_key)
+	file_processes = [ Fileuploader(file_queue, result_queue, endpoint, bucketname, access_key, secret_access_key)
                               for i in range(num_threads) ]			   
 
 	for w in file_processes:
@@ -251,13 +261,10 @@ if __name__ == '__main__':
 		for fname in fileList:
 			file_queue.put((dirName, fname))
 
-	for i in range(num_threads):
+	for i in range(num_threads+5):
 		file_queue.put(None)
 
 
-	#subprocess = Process(target=processRun, args=(file_queue,))
-	#subprocess.start()
-	#subprocess.join()
 	print("Waiting for threads to stop")
 	for w in file_processes:
 		w.join()
